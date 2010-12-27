ff6362b218e3f434f260a4f1a2098899018adfb5
Ville Peurala <ville.peurala@gmail.com>
2010-12-27 18:16:19 +0200

    Submit URB.

--- g13.c
+++ g13.c
@@ -4,7 +4,10 @@
 #include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/types.h>
 #include <linux/usb.h>
+#include <linux/usb/ch9.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Ville Peurala <ville.peurala@gmail.com>");
@@ -36,15 +39,63 @@ MODULE_DEVICE_TABLE(usb, skel_table);
 static irqreturn_t g13_irq_handler(int irq, void *dev_id) {
     printk("Interrupt!\n");
     return IRQ_HANDLED;
-}
+};
+
+static void in_complete(struct urb *urb) {
+    printk("in_complete!\n");
+    printk("status: %d\n", urb->status);
+};
 
 static int skel_probe(struct usb_interface *intf, const struct usb_device_id *id) {
+    struct usb_device *device = interface_to_usbdev(intf);
     struct usb_host_interface *cur_altsetting = intf->cur_altsetting;
     struct usb_interface_descriptor desc = cur_altsetting->desc;
-    int usb_register_dev_result = usb_register_dev(intf, &skel_class);
+    int usb_register_dev_result; 
     int request_irq_result;
+    int i;
+    struct usb_host_endpoint endpoint;
+    struct usb_endpoint_descriptor endpoint_descriptor;
+    __u8 bEndpointAddress;
+    __u8 bmAttributes;
+    __u8 bInterval;
+    struct urb *urb;
+    unsigned int in_pipe;
+    __le16 wMaxPacketSize;
+    void *in_transfer_buffer;
+    int in_transfer_buffer_length;
     printk("probe, %lu\n", id->driver_info);
     printk("cur_altsetting: %d\n", desc.iInterface);
+    for (i = 0; i < desc.bNumEndpoints; i++) {
+        endpoint = cur_altsetting->endpoint[i];
+        endpoint_descriptor = endpoint.desc;
+        bEndpointAddress = endpoint_descriptor.bEndpointAddress;
+        bmAttributes = endpoint_descriptor.bmAttributes;
+        if (usb_endpoint_dir_in(&endpoint_descriptor)) {
+            printk("IN endpoint %d, attributes %d\n", bEndpointAddress, bmAttributes);
+            /* We know that bmAttributes == USB_ENDPOINT_XFER_INT */
+            if (usb_endpoint_xfer_int(&endpoint_descriptor)) {
+                printk("XFER_INT\n");
+                bInterval = endpoint_descriptor.bInterval;
+                wMaxPacketSize = endpoint_descriptor.wMaxPacketSize;
+                in_pipe = usb_rcvintpipe(device, bEndpointAddress);
+                in_transfer_buffer = kmalloc(wMaxPacketSize, GFP_KERNEL);
+                in_transfer_buffer_length = wMaxPacketSize; 
+                urb = usb_alloc_urb(0, GFP_KERNEL);
+                usb_fill_int_urb(urb, device, in_pipe, in_transfer_buffer, in_transfer_buffer_length, &in_complete, NULL, bInterval);
+                usb_submit_urb(urb, GFP_KERNEL);
+            }
+        } else if (usb_endpoint_dir_out(&endpoint_descriptor)) {
+            printk("OUT endpoint %d, attributes %d\n", bEndpointAddress, bmAttributes);
+            /* We know that bmAttributes == USB_ENDPOINT_XFER_INT */
+            if (usb_endpoint_xfer_int(&endpoint_descriptor)) {
+                printk("XFER_INT\n");    
+                bInterval = endpoint_descriptor.bInterval;
+            }
+        } else {
+            printk("BUG!!! Endpoint not IN nor OUT.\n");
+        } 
+    }
+    usb_register_dev_result = usb_register_dev(intf, &skel_class);
     if (usb_register_dev_result ) {
         printk("Not able to get a minor for this device\n");
         usb_set_intfdata(intf, NULL);
